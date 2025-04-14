b0a8396e3593054bc67b8fb9d170e766c01625c1
Ahyun <rladkgus0402@gmail.com>
2025-04-14 16:54:08 +0900

    [feature] 실시간 경매 페이지 미완성

--- src/main/java/farm/farmshop/controller/Live_AuctionController.java
+++ src/main/java/farm/farmshop/controller/Live_AuctionController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Live_AuctionController {
     
-    @GetMapping("/live-auction")
+    @GetMapping("/live_auction")
     public String showLiveAuctionPage() {
         return "live_auction"; // templates/live_auction.html 을 렌더링
     }
