779494089acca4dd4990271c8057d79378014bd1
Ahyun <rladkgus0402@gmail.com>
2025-06-14 22:17:19 +0900

    [fix] 메인페이지 수정 & 신규 경매 흐리게

--- src/main/java/farm/farmshop/controller/MainController.java
+++ src/main/java/farm/farmshop/controller/MainController.java
@@ -68,10 +68,22 @@ public class MainController {
             product.setImageUrl(imageUrl);
         }
 
+        List<Product> topApprovedProducts = approvedProducts.stream()
+                .limit(8)
+                .collect(Collectors.toList());
+
+        List<Product> topCompletedProducts = completedProducts.stream()
+                .limit(8)
+                .collect(Collectors.toList());
+
+        List<Product> topPendingProducts = pendingProducts.stream()
+                .limit(8)
+                .collect(Collectors.toList());
+
         // 모델에 검수 상태에 따른 분리된 리스트 전달
-        model.addAttribute("approvedProducts", approvedProducts); // 실시간 경매 상품
-        model.addAttribute("pendingProducts", pendingProducts);   // 신규 경매 예정 상품
-        model.addAttribute("completedProducts", completedProducts);   // 신규 경매 예정 상품
+        model.addAttribute("topApprovedProducts", topApprovedProducts); // 실시간 경매 상품
+        model.addAttribute("topCompletedProducts", topCompletedProducts);// 신규 경매 예정 상품
+        model.addAttribute("pendingProducts", pendingProducts);// 신규 경매 예정 상품
         
 
         return "main";
